a85e474b21211d3ccdfe9564904b9fd3b7db57fc
Arharov Nikita Mikhailovich <66869636+KinitoSs@users.noreply.github.com>
2023-04-15 11:54:57 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,12 +1,24 @@
+@echo off
+
 echo Starting WinXCorners…
 start WinXCorners.exe
 
 echo Starting CenterTaskbar…
 start CenterTaskbar.exe
 
-echo Starting RoundedTB…
-cd RoundedTB
-start RoundedTB.exe
-cd ..
+echo Checking Windows version…
+ver | findstr /i "10." > nul
+if %errorlevel% == 0 (
+echo Starting Win-10 Virtual Desktop Enhancer…
+start win-10-virtual-desktop-enhancer-master\virtual-desktop-enhancer.exe
+) else (
+ver | findstr /i "Version 11." > nul
+if %errorlevel% == 0 (
+echo Starting Win-11 Virtual Desktop Enhancer…
+start win-11-virtual-desktop-enhancer-master\virtual-desktop-enhancer.exe
+) else (
+echo Windows version not supported.
+)
+)
 
 echo All programs started!
