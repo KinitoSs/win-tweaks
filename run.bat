e88e783540e89c95d4e151c90a5c88705d4f9319
Arharov Nikita Mikhailovich <66869636+KinitoSs@users.noreply.github.com>
2023-04-19 13:46:48 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -10,12 +10,12 @@ echo Checking Windows version…
 ver | findstr /i "10." > nul
 if %errorlevel% == 0 (
 echo Starting Win-10 Virtual Desktop Enhancer…
-start win-10-virtual-desktop-enhancer-master\virtual-desktop-enhancer.exe
+start win-10-virtual-desktop-enhancer\virtual-desktop-enhancer.exe
 ) else (
 ver | findstr /i "Version 11." > nul
 if %errorlevel% == 0 (
 echo Starting Win-11 Virtual Desktop Enhancer…
-start win-11-virtual-desktop-enhancer-master\virtual-desktop-enhancer.exe
+start win-11-virtual-desktop-enhancer\virtual-desktop-enhancer.exe
 ) else (
 echo Windows version not supported.
 )
